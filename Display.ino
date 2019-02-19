40344c0af0c9335102a15c05f74cee9b76e8c583
Juvenal <38957852+JuvsProgramer@users.noreply.github.com>
2019-02-18 20:01:06 -0600

    Add files via upload

---
+++ Display.ino
@@ -0,0 +1,125 @@
+#define A 0
+#define B 1
+#define C 2
+#define D 3
+#define E 4
+#define F 5
+#define G 6
+int delaytime = 1000; 
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(A, OUTPUT);
+  pinMode(B, OUTPUT);
+  pinMode(C, OUTPUT);
+  pinMode(D, OUTPUT);
+  pinMode(E, OUTPUT);
+  pinMode(F, OUTPUT);
+  pinMode(G, OUTPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  uno();
+  delay(delaytime);
+  dos();
+  delay(delaytime);
+  tres(); 
+  delay(delaytime);
+  cuatro();
+  delay(delaytime);
+  cinco();
+  delay(delaytime);
+  seis();
+  delay(delaytime);
+  siete();
+  delay(delaytime);
+  ocho();
+  delay(delaytime); 
+  nueve(); 
+  delay(delaytime);
+  cero();
+  delay(delaytime);
+}
+void uno(){
+  digitalWrite(F, LOW);
+  digitalWrite(E, LOW);
+  digitalWrite(A, HIGH);
+  digitalWrite(B, HIGH);
+  digitalWrite(C, HIGH);
+  digitalWrite(D, HIGH);
+  digitalWrite(G, HIGH);
+}
+void dos(){
+ digitalWrite(A, LOW);
+ digitalWrite(B, LOW);
+ digitalWrite(G, LOW);
+ digitalWrite(E, LOW);
+ digitalWrite(D, LOW);
+ digitalWrite(F, HIGH);
+ digitalWrite(C, HIGH);
+ }
+ void tres(){
+  digitalWrite(A, LOW);
+  digitalWrite(F, LOW);
+  digitalWrite(G, LOW);
+  digitalWrite(E, LOW);
+  digitalWrite(D, LOW);
+  digitalWrite(B, HIGH);}
+void cuatro(){
+digitalWrite(F, LOW);
+digitalWrite(G, LOW);
+digitalWrite(B, LOW);
+digitalWrite(C, LOW);
+digitalWrite(E, HIGH);
+digitalWrite(D, HIGH);
+digitalWrite(A, HIGH);}         
+void cinco(){
+digitalWrite(A, LOW);
+digitalWrite(F, LOW);
+digitalWrite(G, LOW);
+digitalWrite(C, LOW);
+digitalWrite(D, LOW);
+digitalWrite(E, HIGH);
+digitalWrite(B, HIGH);}
+void seis(){
+digitalWrite(A, LOW);
+digitalWrite(F, LOW);
+digitalWrite(G, LOW);
+digitalWrite(C, LOW);
+digitalWrite(E, LOW);
+digitalWrite(D, LOW);
+digitalWrite(B, HIGH);}
+void siete(){
+digitalWrite(A, LOW);
+digitalWrite(B, LOW);
+digitalWrite(G, LOW);
+digitalWrite(C, LOW);
+digitalWrite(D, HIGH);
+digitalWrite(E, HIGH);
+digitalWrite(F, HIGH);}
+void ocho(){
+digitalWrite(A, LOW);
+digitalWrite(B, LOW);
+digitalWrite(C, LOW);
+digitalWrite(D, LOW);
+digitalWrite(E, LOW);
+digitalWrite(F, LOW);
+digitalWrite(G, LOW);}
+void nueve(){
+digitalWrite(A, LOW);
+digitalWrite(B, LOW);
+digitalWrite(F, LOW);
+digitalWrite(G, LOW);
+digitalWrite(E, LOW);
+digitalWrite(C, HIGH);
+digitalWrite(D, HIGH);}
+void cero(){
+digitalWrite(A, LOW);
+digitalWrite(B, LOW);
+digitalWrite(C, LOW);
+digitalWrite(D, LOW);
+digitalWrite(E, LOW);
+digitalWrite(F, LOW);
+digitalWrite(G, HIGH);}                                             
